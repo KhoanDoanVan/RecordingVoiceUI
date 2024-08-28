0c23a3bbf624a551fc87b87ddc7021a546b3923c
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-08-28 22:41:13 +0700

    Initial Commit

---
+++ IOS-RecordingVoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS-RecordingVoice
+//
+//  Created by Đoàn Văn Khoan on 28/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS-RecordingVoice/IOS_RecordingVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_RecordingVoiceApp.swift
+//  IOS-RecordingVoice
+//
+//  Created by Đoàn Văn Khoan on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_RecordingVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
